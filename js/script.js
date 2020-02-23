5516529bafdf597c6e3e9cae704e49d901fe5a92
mamokunni <makuel.mk@gmail.com>
2020-02-24 01:41:36 +0300

    add form validation for submit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
-$(document).ready(function(){
-  $(".clickable1").click(function(){
+$(document).ready(function() {
+  $(".clickable1").click(function() {
     $(".image1").toggle();
     $(".whatwedo1").toggle();
+  });
+
+  $(".clickable2").click(function() {
+    $(".image2").toggle();
+    $(".whatwedo2").toggle();
+  });
+
+  $(".clickable3").click(function() {
+    $(".image3").toggle();
+    $(".whatwedo3").toggle();
+  });
+
+
+
+  $("form").submit(function() {
+    event.preventDefault();
+    var name = document.getElementById('name')
+    var email = document.getElementById('email')
+    var send = "Your Message is submitted"
+    if (name.value === '' || name.value == null) {
+      alert("Fill the form");
+    } else {
+      alert(send);
+    }
+
+
+  });
 });
-
-$(".clickable2").click(function(){
-  $(".image2").toggle();
-  $(".whatwedo2").toggle();
-});
-
-});
-
-
-
-
-
-
-
-
-
-
