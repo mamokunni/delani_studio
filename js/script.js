ffe58a5ca7191d06ef2cb1a57cccb4377a4d733a
mamokunni <makuel.mk@gmail.com>
2020-02-24 00:00:19 +0300

    add function for toggle on mouse click whatwedo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+  $(".clickable1").click(function(){
+    $(".image1").toggle();
+    $(".whatwedo1").toggle();
+});
 
+$(".clickable2").click(function(){
+  $(".image2").toggle();
+  $(".whatwedo2").toggle();
+});
 
 });
+
+
+
+
+
+
+
+
+
+
